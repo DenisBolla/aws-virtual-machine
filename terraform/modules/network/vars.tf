a2093fcf9ab0d6a3b6ac1f68a937caef3d1447c2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-18 21:03:31 -0300

    Re-ajuste Pipeline

--- terraform/modules/network/vars.tf
+++ terraform/modules/network/vars.tf
@@ -1,18 +1,15 @@
-# CIDR da VPC
 variable "vpc_cidr" {
   description = "CIDR block da VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
-# CIDR da Subnet pública
 variable "public_subnet_cidr" {
   description = "CIDR block da Subnet pública"
   type        = string
   default     = "10.0.1.0/24"
 }
 
-# Nome do Security Group
 variable "sg_name" {
   description = "Nome do Security Group público"
   type        = string
