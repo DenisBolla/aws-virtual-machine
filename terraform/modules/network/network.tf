821870ede67f3f2a073abc886bc2ad332e13ac2f
DenisBolla <denisvini@gmail.com>
2025-09-19 02:04:51 -0300

    Ajustes pipeline

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -27,7 +27,7 @@ resource "aws_route_table" "public" {
   }
 }
 
-# SUBNET ASSOCIATION
+
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
